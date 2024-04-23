8e3e1d242d88c9bca1c894b1cc1d848d9f544524
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2024-04-24 00:13:49 +0600

    initial setup

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 import express from "express";
 
 const app = express();
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+    return res.status(200).send({
+        success: "OK",
+        message: "Hello World",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`APP is running on port ${PORT}`);
+});
